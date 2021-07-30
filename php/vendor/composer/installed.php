fe89e1d6975a263bb543e696ca1ad908a036c2b3
CodeCanna <codecannamw@gmail.com>
2021-07-30 12:19:10 -0600

    Imports are currently broken.

---
+++ php/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '1fec80ebcf8c6ef96d4b7c2ae537a421c7e4ad6d',
+    'name' => 'greenthumb/php',
+  ),
+  'versions' => 
+  array (
+    'greenthumb/php' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1fec80ebcf8c6ef96d4b7c2ae537a421c7e4ad6d',
+    ),
+  ),
+);
