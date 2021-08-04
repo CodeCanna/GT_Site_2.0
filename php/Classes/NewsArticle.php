b9a06177831802bf49a048d601af290e670e90a9
CodeCanna <codecannamw@gmail.com>
2021-08-03 21:26:43 -0600

    Added picture upload functionality.

--- php/Classes/NewsArticle.php
+++ php/Classes/NewsArticle.php
@@ -1,61 +1,79 @@
 <?php
+
 namespace GreenThumb\php;
 
 use ArgumentCountError;
 use DateTime;
+use Exception;
 use InvalidArgumentException;
 use TypeError;
 
 use function PHPSTORM_META\type;
 
+// Define our upload directory
+define('UPLOAD_DIR', dirname(__DIR__, 2) . "/resources/images");
+
 /**
+ * This class represents a GreenThumb news/product update article
  * 
+ * @var string $articleId
+ * @var DateTime $articleDate
+ * @var string $articleTitle
+ * @var string $articleContent
+ * @var array $articleImages
  */
-class NewsArticle {
+class NewsArticle
+{
     private $articleId;
     private $articleDate;
     private $articleTitle;
     private $articleContent;
-    
+    private $articleImages;
+
     /**
      * @param int $articleId
      * @param DateTime $articleDate
      * @param string $articleTitle
      * @param string $articleContent
+     * @param array $articleImages
      */
-    function __construct(string $articleId, DateTime $articleDate, string $articleTitle, string $articleContent, array $articleImages) {
+    function __construct(string $articleId, DateTime $articleDate, string $articleTitle, string $articleContent, array $articleImages)
+    {
         $this->setArticleId($articleId);
         $this->setArticleDate($articleDate);
         $this->setArticleTitle($articleTitle);
         $this->setArticleContent($articleContent);
+        $this->setArticleImages($articleImages);
     }
 
     /**
      * @return int
      */
-    public function getArticleId(): string {
+    public function getArticleId(): string
+    {
         return $this->articleId;
     }
 
     /**
      * @param $articleId
      */
-    protected function setArticleId(string $articleId): void {
+    protected function setArticleId(string $articleId): void
+    {
         try {
             if (empty($articleId)) {
                 throw new InvalidArgumentException();
             }
-            
+
             if (gettype($articleId) != 'string') {
                 throw new TypeError();
             }
 
             // Set article id
             $this->articleId = $articleId;
-        } catch(InvalidArgumentException $e) {
+        } catch (InvalidArgumentException $e) {
             echo $e;
             echo "Article ID cannot be empty...";
-        } catch(TypeError $e) {
+        } catch (TypeError $e) {
             echo $e;
             echo "Article ID must be an int.\n";
             echo "$articleId was given...";
@@ -65,27 +83,29 @@ class NewsArticle {
     /**
      * @return DateTime
      */
-    public function getArticleDate(): DateTime | null {
+    public function getArticleDate(): DateTime | null
+    {
         // We can return null if its empty so we know whats going on.
         if (empty($this->articleDate)) {
             return null;
         }
         return $this->articleDate;
     }
-    
+
     /**
      * @param $articleDate
      */
-    protected function setArticleDate(DateTime $articleDate) {
+    protected function setArticleDate(DateTime $articleDate)
+    {
         try {
             // Make sure our article date isn't empty
             if (empty($articleDate)) {
                 throw new InvalidArgumentException();
             }
             $this->articleDate = $articleDate;
-        } catch(ArgumentCountError) {
+        } catch (ArgumentCountError) {
             echo "The article date wasn't passed...";
-        } catch(InvalidArgumentException) {
+        } catch (InvalidArgumentException) {
             echo "The article date cannot be empty...";
         }
     }
@@ -93,23 +113,25 @@ class NewsArticle {
     /**
      * @return string
      */
-    public function getArticleTitle(): string {
+    public function getArticleTitle(): string
+    {
         return $this->articleTitle;
     }
 
     /**
      * @param $articleTitle
      */
-    protected function setArticleTitle(string $articleTitle): void {
+    protected function setArticleTitle(string $articleTitle): void
+    {
         try {
             if (empty($articleTitle)) {
                 throw new InvalidArgumentException();
             }
             $this->articleTitle = $articleTitle;
-        } catch(ArgumentCountError $e) {
+        } catch (ArgumentCountError $e) {
             echo $e;
             echo "Article title wasn't passed...";
-        } catch(InvalidArgumentException $e) {
+        } catch (InvalidArgumentException $e) {
             echo $e;
             echo "Article title cannot be empty...";
         }
@@ -118,38 +140,76 @@ class NewsArticle {
     /**
      * @return string
      */
-    public function getArticleContent(): string {
+    public function getArticleContent(): string
+    {
         return $this->articleContent;
     }
 
     /**
      * @param $articleContent
      */
-    protected function setArticleContent(string $articleContent) {
+    protected function setArticleContent(string $articleContent)
+    {
         try {
             if (empty($articleContent)) {
                 throw new InvalidArgumentException();
             }
             $this->articleContent = $articleContent;
-        } catch(ArgumentCountError $e) {
+        } catch (ArgumentCountError $e) {
             echo $e;
             echo "Article content wasn't passed...";
-        } catch(InvalidArgumentException $e) {
+        } catch (InvalidArgumentException $e) {
             echo $e;
             echo "Article content must not be emtpy...";
         }
     }
 
+    public function getArticleImages(): array
+    {
+        return $this->articleImages;
+    }
+
     /**
+     * This function is a little tricky.  The stucture of the $_FILES array determines how this function was written.
+     * it parses the initial $_FILES array and creates a simpliefied version of the array, then it moves the files to the upload directory storing the file path in the 
+     * php object
+     * 
      * @param array $imgArray
      */
-    public function setArticleImages(array $imgArray): void
+    protected function setArticleImages(array $imgArray): void
     {
-        if (! empty($imgArray)) {
-            foreach($imgArray as $img)
-            {
-                echo $img;
+        try {
+            if (!empty($imgArray)) {
+                // Check the number of files sent
+                if (sizeof($imgArray) > 5) {
+                    throw new Exception("Too many images sent, send 5 or less images...");
+                }
+
+                // Parse the image array
+                foreach ($imgArray as $img => $imgData) {
+                    $this->articleImages[$imgData['name']] = $imgData['tmp_name'];
+                }
+            } else {
+                // Set articleImages to NULL if none are found
+                $this->articleImages = NULL;
             }
+
+            // Check that our uploads directory exists
+            if (!is_dir(UPLOAD_DIR)) {
+                throw new Exception("Can't find the uploads directory, looking in make sure it exists...");
+            }
+
+            // Move uploaded files to the news uploads directory based on file type
+            foreach ($this->articleImages as $img => $imgPath) {
+                if (pathinfo(UPLOAD_DIR."$img", PATHINFO_EXTENSION) === 'jpg' || pathinfo(UPLOAD_DIR."$img", PATHINFO_EXTENSION) === 'jpeg')
+                {
+                    move_uploaded_file($imgPath, UPLOAD_DIR . "/jpg/" . $img);
+                } else {
+                    move_uploaded_file($imgPath, UPLOAD_DIR . "/png/" . $img);
+                }
+            }
+        } catch (Exception $e) {
+            echo $e;
         }
     }
 
@@ -158,7 +218,8 @@ class NewsArticle {
     /**
      * 
      */
-    public function getAllArticles($db) {
+    public function getAllArticles($db)
+    {
         echo 'all thingies!!';
     }
 }
