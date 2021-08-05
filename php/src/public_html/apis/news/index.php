e9b25dc380e3f9f3d7067afdca2337f7c4a5445a
CodeCanna <codecannamw@gmail.com>
2021-08-04 20:34:19 -0600

    Data now hits my test database from the api.

--- php/src/public_html/apis/news/index.php
+++ php/src/public_html/apis/news/index.php
@@ -1,7 +1,9 @@
 <?php
 require_once(dirname(__DIR__, 4) . "/vendor/autoload.php");
 require_once(dirname(__DIR__, 4) . "/Classes/NewsArticle.php");
+require_once(dirname(__DIR__, 4) . "/Classes/DBConnection.php");
 
+use GreenThumb\php\DBGTConnection;
 use GreenThumb\php\NewsArticle;
 
 // Create empty reply
@@ -13,19 +15,36 @@ try {
     // Get the request method
     $method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
+    // Connect to the DB
+    $db = new DBGTConnection('secrets.json');
+
+    if (! $db->checkConnection()) {
+        throw new Exception("There was a problem connecting to the database...");
+    }
+
     if ($method === 'GET') {
         echo "GET";
     } elseif ($method === 'POST') {
         // Create our news article
-        $articleId = uniqid(); // Generate ID for article
+        $articleId = uniqid('article', true); // Generate ID for article
         $articleDateTime = new DateTime();
+
+        // Datetime to string
+        $articleDateTimeStr = $articleDateTime->format('Y-m-d H:i:s');
         // Get the content from our $_POST array
         $articleTitle = $_POST['articleTitle'];
         $articleContent = $_POST['articleContent'];
 
         $newArticle = new NewsArticle($articleId, $articleDateTime, $articleTitle, $articleContent, $_FILES);
 
-        var_dump($newArticle);
+        $articleImagesStr = serialize($newArticle->getArticleImages());
+        
+        //Store our data
+        $db->query("INSERT INTO news_articles (articleId, articleDate, articleTitle, articleContent, articleImages) VALUES ('$articleId', '$articleDateTimeStr', '$articleTitle','$articleContent', '$articleImagesStr')");
+
+        var_dump($db);
+
+        $db->close();
     }
 } catch (Exception $e) {
     echo $e;
